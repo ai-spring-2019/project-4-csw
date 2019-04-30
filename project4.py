56ab573cea6c8da5773c394aa608fec8cb18d9e9
cswahl <47008365+cswahl@users.noreply.github.com>
2019-04-30 14:13:52 -0400

    untested. typed into browser.

--- project4.py
+++ project4.py
@@ -1,5 +1,7 @@
 """
-PLEASE DOCUMENT HERE
+NAME: Colleen Wahl
+DATE: ?
+PROJECT:
 
 Usage: python3 project3.py DATASET.csv
 """
@@ -79,8 +81,99 @@ def accuracy(nn, pairs):
 
 ################################################################################
 ### Neural Network code goes here
+class NetNode():
+    """ A node in the neural network. """
+    
+    def __init__(self, a = 'None', error = 'None'):
+        
+        self._a = a
+        self.error = error
+        self.parents = []
+        self.p_weights = []
+        self.children = []
+        self.c_weights = []
+    
+    def set_a(self, a):
+        self._a = a
+        
+    def get_a(self):
+        return self._a
+    
 
+class NeuralNetwork():
+    def __init__(self, node_num_lst, alpha = 0.1, decrease = False):
+        
+        self.alpha = alpha
+        
+        # determines whether alpha will get smaller as we go
+        self.decrease = decrease
+        
+        # each item in the list is the number of nodes in that layer,
+        # so node_num_lst[0] is the # of nodes in the first layer, etc.
+        dummy = NetNode(1)
+        self.in_nodes = [dummy]
+        for _ in range(node_num_lst[0]):
+            self.in_nodes.append(NetNode())
+                         
+        # contains nodes in hidden layers and the output layer
+        self.other_nodes = []
+        for i in range(1, len(node_num_lst)):
+            this_layer = []
+            for _ in range(node_num_lst[i]):
+                new_node = NetNode()
+                this_layer.append(new_node)
+                if i == 1:
+                    for p_node in self.in_nodes:
+                        self.connect(p_node, new_node)
+                else:
+                    for p_node in self.other_nodes[i-2]:
+                        self.connect(p_node, new_node)
+            self.other_nodes.append(this_layer)
+            
+        self.out_nodes = self.other_nodes[-1]
+                
+        
+    def connect(self, n1, n2, weight = 'None'):
+        """ Connect nodes n1 and n2 with a path of the given weight.
+            Assumes n1 is the parent node. """
+        
+        if weight == 'None':
+            weight = random.random()
 
+        n1.children.append(n2)
+        n1.c_weights.append(weight)
+        n2.parents.append(n1)
+        n2.p_weights.append(weight)
+            
+    def predict_class(self):
+        pass
+    
+    def forward_propagate(self, input_lst):
+        """ Given a list of inputs, determine all nodes' activations. """
+        
+        for i in range(len(input_lst)):
+            # add 1 to i to skip over the dummy node.
+            # set activations for input nodes = to the input.
+            self.in_nodes[i+1].set_a(input_lst[i])
+            
+        for layer in self.other_nodes:
+            for node in layer:
+                in_j = 0
+                for t in range(len(node.parents)):
+                    inj += p_weights[t] * parents[t].get_a()
+                node.set_a(logistic(in_j))
+    
+    def back_propagate(self, output_lst):
+        for j in len(self.out_nodes):
+            # must actually calculate in_j
+            in_j = 0
+            
+            curr_node = self.out_nodes[j]
+            temp_value = logistic(in_j)/curr_node.get_a()
+            curr_node.error = temp_value * (1 - temp_value) * (output_lst[j] - curr_node.get_a())
+            
+        # now do similar thing for mid-layer nodes
+    
 
 
 
